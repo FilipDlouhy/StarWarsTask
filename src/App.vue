dddf7783d4a9931b54f2619ef9d70379ff0cccbe
Phillipe Augustus <fi.dlo@email.cz>
2023-04-12 00:13:31 +0200

    Data sampels added

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,15 @@
     <nav class="navbar">
       <div>
         <input type="text" placeholder="Search ship by name">
+                <select>
+          <option value="people">All</option>
+          <option value="people">Characters</option>
+          <option value="planets">Planets</option>
+          <option value="starships">Starships</option>
+          <option value="vehicles">Vehicles</option>
+          <option value="species">Species</option>
+          <option value="films">Films</option>
+        </select>
       </div>
       <div>
         <h1>The Star Wars Hangar</h1>
@@ -15,8 +24,125 @@
     </nav>
 
 
-    <div class="ShipsContent">
-      <!-- content div here -->
+    <div class="DataContent">
+      <div class="Ship">
+        <div>
+          <h1>Sentinel-class landing craft</h1>
+          <p>Model:Imperial I-class Star Destroyer </p>
+        </div>
+        <div>
+          <p>manufacturer: Kuat Drive Yards</p>
+          <p>crew: 47,060 people</p>
+          <p>Type: Ship </p>
+        </div>
+        <div>
+          <button>Mark as favorite</button>
+          <button>Show More</button>
+          <button>Delete</button>
+        </div>
+      </div>
+
+
+
+            <div class="Ship">
+        <div>
+          <h1>Sand Crawler</h1>
+          <p>Model:Digger Crawler</p>
+        </div>
+        <div>
+          <p>manufacturer: Sienar Fleet Systems</p>
+          <p>crew: 1 people</p>
+          <p>Type: Vehicle </p>
+        </div>
+        <div>
+          <button>Mark as favorite</button>
+          <button>Show More</button>
+          <button>Delete</button>
+        </div>
+      </div>
+
+
+            <div class="Ship">
+        <div>
+          <h1>Human</h1>
+          <p>Classification:mammal</p>
+        </div>
+        <div>
+          <p>manufacturer: Kuat Drive Yards</p>
+          <p>Average Height: 180</p>
+          <p>Type: Species </p>
+        </div>
+        <div>
+          <button>Mark as favorite</button>
+          <button>Show More</button>
+          <button>Delete</button>
+        </div>
+      </div>
+
+
+
+
+
+            <div class="Ship">
+        <div>
+          <h1>A New Hope</h1>
+          <p>Episode: 4 </p>
+        </div>
+        <div>
+          <p>Director: George Lucas</p>
+          <p>Producer: Gary Kurtz, Rick McCallum</p>
+          <p>Type: Film  </p>
+        </div>
+        <div>
+          <button>Mark as favorite</button>
+          <button>Show More</button>
+          <button>Delete</button>
+        </div>
+      </div>
+
+
+
+
+            <div class="Ship">
+        <div>
+          <h1>Luke Skywalker</h1>
+          <p>Birth year: 19BBY </p>
+        </div>
+        <div>
+          <p>Gender: Male</p>
+          <p>Skin color: fair</p>
+          <p>Type: Character </p>
+        </div>
+        <div>
+          <button>Mark as favorite</button>
+          <button>Show More</button>
+          <button>Delete</button>
+        </div>
+      </div>
+
+
+
+            <div class="Ship">
+        <div>
+          <h1>Tatooine</h1>
+          <p>Diameter:10465 Km </p>
+        </div>
+        <div>
+          <p>Terrain: Desert</p>
+          <p>Population: 200000 people</p>
+          <p>Type: Planet  </p>
+        </div>
+        <div>
+          <button>Mark as favorite</button>
+          <button>Show More</button>
+          <button>Delete</button>
+        </div>
+      </div>
+
+
+
+
+
     </div>
 
   </div>
@@ -39,29 +165,73 @@
 @mixin flex-ships {
   display: flex;
   align-items: flex-start;
+  justify-content: center;
   flex-wrap: wrap;
 }
 .container {
   width: 100vw;
   height: 100vh;
+  overflow-x: hidden;
 }
-  .ShipsContent {
+  .DataContent {
     @include flex-ships();
     overflow-x: hidden;
     overflow-y: auto;
     width: 100%;
+    padding:  0 100px;
 
-    >div
-    {
-      width: 300px;
-      height:  300px;
-      
+  }
+
+.Ship {
+  width: 350px;
+  height: 350px;
+  background-color: rgba(0, 0, 0, 0.897);
+  margin: 20px;
+  padding: 10px;
+  border-radius: 5px;
+  > div {
+    width: 100%;
+    color: white;
+    > p {
+      word-spacing: 3px;
+      font-weight: 700;
+    }
+    > button {
+      width: 150px;
+      height: 25px;
+      margin: 5px;
+      font-weight: 700;
+      transition: 0.4s;
+      cursor: pointer;
+    }
+    > h1 {
+      font-size: 1.4rem;
+      font-weight: 900;
     }
   }
+  > div:nth-child(1),
+  > div:nth-child(2) {
+    height: 40%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+  }
+  > div:nth-child(3) {
+    height: 20%;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    flex-wrap: wrap;
+  }
+}
+
+
+
 
 .navbar {
   width: 100%;
-  background-color: black;
+  background-color: rgba(0, 0, 0, 0.87);
   height: 100px;
   display: flex;
   justify-content: space-between;
@@ -97,7 +267,7 @@
 
     > input[type="text"] {
       width: 300px;
-      height: 35px;
+      height: 30px;
       padding: 0.5rem;
       border-radius: 2px;
       border: 1px solid #ccc;
@@ -112,6 +282,19 @@
     > input[type="text"]:focus {
       box-shadow: 2px 2px 5px white;
     }
+    > select {
+      width: 200px;
+      height: 30px;
+      background-color: #f1f1f1;
+      border: none;
+      font-size: 1.2rem;
+      border-radius: 2px;
+      margin-right: 0.5rem;
+      font-weight: 500;
+      cursor: pointer;
+      transition: 0.4s;
+    }
+
   }
 }
 </style>
